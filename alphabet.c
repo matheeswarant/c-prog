4db53269cb53674308eab3ed9023210a49ef9202
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-19 16:07:14 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+printf("enter the letter");
+scanf("%d",&ch);
+if(ch>='a'&&ch<='z');
+{
+printf("letter is alphabet");
+}
+else
+{
+printf("letter is not alphabet"):
+}}
