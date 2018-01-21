0f37370e55905121bfe1d83e4d7e0533b77b0909
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-21 03:59:19 +0000

    Update count.c

--- count.c
+++ count.c
@@ -3,13 +3,13 @@ void main()
 {
 int n,a[i],i,k;
 printf("enter the number");
-for(i=0;i<+n;i++)
+for(i=0;i<=n;i++)
 {
 scanf("%d",&a[i]);
 }
 printf("enter the limit");
 scanf("%d",&k);
-for(i=0;i<+k;i++)
+for(i=0;i<=k;i++)
 {
 sum=sum+a[i];
 }
