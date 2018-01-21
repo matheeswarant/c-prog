f7d5979f49b57e5860866d03bccb84f5a60610ff
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-21 04:13:30 +0000

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int n,sum=0;
+int n;
 printf("enter the number");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-sum=sum+1;
-}
-printf("enter the num"):
+sum=n+(n+1)%2;
+printf("%d,s"):
 }
